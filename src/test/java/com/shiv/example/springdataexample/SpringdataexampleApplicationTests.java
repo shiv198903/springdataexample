92d8fbfe7f2aa3f367f18bfb590f26c4c1bc7dbc
shiv <shivakumarsuryan@gmail.com>
2019-10-07 11:11:29 +0530

    Artifacts for Spring Boot Application

---
+++ src/test/java/com/shiv/example/springdataexample/SpringdataexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shiv.example.springdataexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringdataexampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
